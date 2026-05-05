e6db878407b1b68af030f4274feac59b2e4364db
Lei Shi <mleoshi@gmail.com>
2026-05-05 14:53:00 +0800

    Initial Commit

---
+++ Livescript/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Livescript
+//
+//  Created by Lei S on 2026/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Livescript/LivescriptApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivescriptApp.swift
+//  Livescript
+//
+//  Created by Lei S on 2026/5/5.
+//
+
+import SwiftUI
+
+@main
+struct LivescriptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
